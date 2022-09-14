ab7bb132f26e30177ef3b521e929a9f12fe3e6a2
George Gkionis <toymaker@vivawallet.com>
2022-09-14 22:14:06 +0300

    Playing with updates

--- src/BeneficiaryNameValidationSvc/WeatherForecast.cs
+++ src/BeneficiaryNameValidationSvc/WeatherForecast.cs
@@ -6,9 +6,9 @@ public class WeatherForecast
 {
     public DateTime Date { get; set; }
 
-    public int TemperatureCC { get; set; }
+    public int TemperatureC { get; set; }
 
-    public int TemperatureFF => 32 + (int)(TemperatureCC / 0.5556);
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
     public string? Summary { get; set; }
 }
