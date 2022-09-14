b33e1dca80632a307f1c5006a1ec8dad95838fb0
George Gkionis <toymaker@vivawallet.com>
2022-09-14 21:03:27 +0300

    Trying to re-deploy & update

--- src/BeneficiaryNameValidationSvc/Program.cs
+++ src/BeneficiaryNameValidationSvc/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationMonitoring();
 
 var app = builder.Build();
 
@@ -20,7 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
